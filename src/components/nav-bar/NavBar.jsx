299c4a0179012e572ac4e521e7529a91973795b0
IreneFelice <i.f.buijing@gmail.com>
2024-12-12 10:30:06 +0100

    Add navigation links

--- src/components/nav-bar/NavBar.jsx
+++ src/components/nav-bar/NavBar.jsx
@@ -1,7 +1,14 @@
+import { NavLink } from 'react-router-dom';
+
 function NavBar () {
     return(
     <>
-        <div>Navigation</div>
+        <ul>
+            <li><NavLink to="/" className={({isActive}) => isActive ? 'active' : 'default'}>Kitchen</NavLink></li>
+            <li><NavLink to="/recipe-book" className={({isActive}) => isActive ? 'active' : 'default'}>My recipe book</NavLink></li>
+            <li><NavLink to="/login" className={({isActive}) => isActive ? 'active' : 'default'}>Login</NavLink></li>
+            <li><NavLink to="/for-parents" className={({isActive}) => isActive ? 'active' : 'default'}>For Parents</NavLink></li>
+        </ul>
     </>
     )
 }
